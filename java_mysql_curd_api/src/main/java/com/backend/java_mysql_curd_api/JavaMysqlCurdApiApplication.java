db73fa703405059a49edbcf8bcb8a9b1edab6afd
Emile-D-Bouwer <110655304+Emile-D-Bouwer@users.noreply.github.com>
2022-08-23 13:53:02 +0200

    Bare Bones Java-MySQL CURD API
    Just the framework.  The useful bits are still coming.

---
+++ java_mysql_curd_api/src/main/java/com/backend/java_mysql_curd_api/JavaMysqlCurdApiApplication.java
@@ -0,0 +1,13 @@
+package com.backend.java_mysql_curd_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMysqlCurdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMysqlCurdApiApplication.class, args);
+	}
+
+}
---
+++ java_mysql_curd_api/src/test/java/com/backend/java_mysql_curd_api/JavaMysqlCurdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.java_mysql_curd_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMysqlCurdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
